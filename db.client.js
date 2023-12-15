60a47974cfefac7c30672b2703451b6e07267c20
bakslamenace <mahmoud.elm@icloud.com>
2023-12-15 10:48:32 +0100

    Fix of the variables

--- db.client.js
+++ db.client.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize')
 
 const DB_URL = process.env.DATABASE_URL;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_USER = process.env.DB_USER;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 const DB_PORT = process.env.DB_PORT
