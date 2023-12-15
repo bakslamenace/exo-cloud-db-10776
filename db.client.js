2373d7d51770acaa45124231d038d3e899278e75
bakslamenace <mahmoud.elm@icloud.com>
2023-12-15 10:58:34 +0100

    Fix of the variables

--- db.client.js
+++ db.client.js
@@ -3,13 +3,12 @@ const { Sequelize } = require('sequelize')
 const DB_URL = process.env.DATABASE_URL;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_USER = process.env.DB_USER;
-const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 const DB_PORT = process.env.DB_PORT;
 
 // database
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+  host: DB_URL,
   port: DB_PORT,
   dialect: 'postgres',
   dialectOptions: {
