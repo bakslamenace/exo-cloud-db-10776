4bf800f5251cabc13d437f2b3dc79192a52010aa
bakslamenace <mahmoud.elm@icloud.com>
2023-12-15 10:52:50 +0100

    Fix of the variables

--- db.client.js
+++ db.client.js
@@ -5,7 +5,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_USER = process.env.DB_USER;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
-const DB_PORT = process.env.DB_PORT
+const DB_PORT = process.env.DB_PORT;
 
 // database
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
